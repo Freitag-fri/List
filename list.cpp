dace972eb402d37cb8e0bc38bd99ff68f8ded752
Sanya Zhitnik <sanya007ua@gmail.com>
2019-06-22 23:53:32 +0300

    addBack

--- list.cpp
+++ list.cpp
@@ -4,9 +4,6 @@ using namespace std;
 
 class List
 {
-public:
-
-
 private:
 
     class Element
@@ -15,12 +12,61 @@ private:
         Element *next;
         int data;
 
-        Element (Element *Knext, int Kdata)
+        Element (int Kdata = 0, Element *Knext = nullptr)
         {
          next = Knext;
          data = Kdata;
         }
-
     };
+
+    int size;
+public:
+    Element *firstElement;
+    void addElement(int data);
+    void addElement2(int data);
+    int GetSize() {return size;}
+
+    List ()
+    {
+      size = 0;
+      firstElement = nullptr;
+    }
 };
 
+void List::addElement2(int data)
+{
+  if(firstElement->next == nullptr)
+  {
+     firstElement = new Element (data);
+  }
+
+  else
+  {
+      Element *buffer = firstElement;
+      while(buffer->next != nullptr)
+      {
+          buffer = buffer->next;
+      }
+      buffer->next = new Element(data);
+  }
+size++;
+}
+
+void List::addElement(int data)
+{
+    if (firstElement == nullptr)
+    {
+       firstElement = new Element(data);
+    }
+    else
+    {
+      Element *buffer = firstElement;
+      while(buffer->next != nullptr)
+      {
+        buffer = buffer->next;
+
+      }
+        buffer->next = new Element(data);
+    }
+   size ++;
+}
--- main.cpp
+++ main.cpp
@@ -5,30 +5,44 @@ using namespace std;
 
 int *foo(int *oldarr, int n, int q);
 
+
+
+
 int main(int argc, char *argv[])
 {   QCoreApplication a(argc, argv);
 
     List b;
 
+    b.addElement2(5);
+    b.addElement2(10);
+    b.addElement2(15);
 
-    int n = 1;
-    int q;
-    int *arr = new int[n];
-    cin >> q;
-    arr[n - 1] = q;
-
-    while(true)
+    for(int i = 0; i < b.GetSize(); i++)
     {
-        cin >> q;
-        arr = foo(arr , n, q);
-        n++;
-        for (int i = 0; i < n; i++)
-        {
-            cout << arr[i] << ", ";
-        }cout << endl;
+      cout << b.firstElement;
     }
+
+
+//    int n = 1;
+//    int q;
+//    int *arr = new int[n];
+//    cin >> q;
+//    arr[n - 1] = q;
+
+//    while(true)
+//    {
+//        cin >> q;
+//        arr = foo(arr , n, q);
+//        n++;
+//        for (int i = 0; i < n; i++)
+//        {
+//            cout << arr[i] << ", ";
+//        }cout << endl;
+//    }
     return a.exec();
 }
+
+
 
 
 int *foo(int *oldarr, int n, int q)
